9eb46d8148de15a5566cdbd293ef087b303f3373
LeleGuidi <emanuele.guidi98@outlook.it>
2022-06-21 15:32:20 +0200

    Richiesta parola censurata e ristampa paragrafo

--- index.php
+++ index.php
@@ -1,20 +1,35 @@
 <?php
     //Creazione variabile con paragrafo
     $paragrafo = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem odio voluptatem quo, quia tenetur iure alias, modi delectus rerum id nesciunt assumenda officiis porro cupiditate nisi vero suscipit vitae? Laboriosam!';
-
+    $censura = $_GET["censura"];
+    $paragrafo_censurato = str_replace($censura, "***", $paragrafo);
+    $lunghrezza_nuovo_paragrafo = strlen($paragrafo_censurato);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Badwords</title>
 </head>
 <body>
+    <!-- Stampa del paragrafo e lunghezza -->
     <h1>Paragrafo:</h1>
     <p><?= $paragrafo ?></p>
     <h2>Lunghezza paragrafo:</h2>
     <span><?= strlen($paragrafo) ?></span>
+    <!-- Form per richiedere la parola da censurare  -->
+    <form action="index.php" method="get">
+        <h2>Scegli la parola da censurare</h2>
+        <input type="text" name="censura">
+        <input type="submit">
+    </form>
+    <!-- Stampa del paragrafo censurato e lunghezza  -->
+    <h1>Paragrafo con censure:</h1>
+    <p><?= $paragrafo_censurato?></p>
+    <h2>Lunghezza nuovo paragrafo censurato:</h2>
+    <span><?= $lunghrezza_nuovo_paragrafo?></span>
 </body>
 </html>
